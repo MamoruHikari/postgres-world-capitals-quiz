d68dacb3badfd964aa35afb9bb5f14aacb86af31
MamoruHikari <173257494+MamoruHikari@users.noreply.github.com>
2025-01-20 18:45:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "sekai",
-  password: "japanfuture",
-  port: 5432
+  connectionString: 'postgresql://postgresdb_owner:XLNl3T5gmeOB@ep-frosty-dew-a2nbh26t.eu-central-1.aws.neon.tech/postgresdb?sslmode=require',
 });
 
 db.connect();
