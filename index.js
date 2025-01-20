377581362c31e51ff614b9cddc5af94f747ef3e0
MamoruHikari <173257494+MamoruHikari@users.noreply.github.com>
2025-01-20 19:32:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 const db = new pg.Client({
-  connectionString: 'postgresql://postgresdb_owner:XLNl3T5gmeOB@ep-frosty-dew-a2nbh26t.eu central-1.aws.neon.tech/postgresdb?sslmode=require',
+  connectionString: 'postgresql://postgresdb_owner:XLNl3T5gmeOB@ep-frosty-dew-a2nbh26t.eu-central-1.aws.neon.tech/postgresdb?sslmode=require',
 });
 
 db.connect();
